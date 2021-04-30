59b9e92ed683a58746922797788a7a2e04a0f462
Richard Balogh <logbasan@gmail.com>
2021-04-30 12:52:05 +0200

    sorry

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,11 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    document.querySelectorAll('#link-list');
+    if(document.innerHtml = 'outer-link'){
+        addEventListener('click', function() {
+            document.target = '_blank';
+        })
+    }
+}
 
 
 export { changeOuterLinks };
